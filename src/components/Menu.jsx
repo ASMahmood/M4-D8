3a79555aebd239990d29952ceae01b9acb017d9a
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-25 14:10:22 +0000

    added an alert for if the dishes array is empty

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import dishes from "../data/menu.json";
 import SingleDish from "./SingleDish.jsx";
-import { Container } from "react-bootstrap";
+import { Alert, Container } from "react-bootstrap";
 
 const Menu = () => (
   <Container>
     {dishes && dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
+    {dishes.length === 0 && (
+      <Alert className="mt-5" variant="warning">
+        Work In Progress
+      </Alert>
+    )}
   </Container>
 );
 export default Menu;
